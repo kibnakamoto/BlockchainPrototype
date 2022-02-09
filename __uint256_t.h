0c66ae8446a437464cc5732912b1a8796dbf288c
kibnakamoto <86175813+kibnakamoto@users.noreply.github.com>
2022-02-09 17:44:08 -0500

    Create __uint256_t.h

---
+++ __uint256_t.h
@@ -0,0 +1,13 @@
+#include <stdint.h>
+
+class IntTypes
+{
+    public:
+        /* this 256-bit unsigned int turns each transaction of the mempool 
+           into 1 numerical variable */
+        inline std::pair<__uint128_t, __uint128_t> __uint256_t(uint64_t* mempoolSingleHash)
+        {
+            
+            return {};
+        }
+};
