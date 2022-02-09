0d8e7a5a1920c7605e3ce8f02453d17c5da0c4fa
kibnakamoto <86175813+kibnakamoto@users.noreply.github.com>
2022-02-09 18:33:58 -0500

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,3 +1,9 @@
+/*
+* This implementation is for modern c++. Some of the methods used are after c++17
+* so if you are using an older version, make some tweaks.
+* 
+*/
+
 #include <iostream>
 #include <string>
 #include "__uint256_t.h"
@@ -8,10 +14,12 @@ int main()
     /* iterate each value of mempool using this method, replace string abc 
        with the that needs to be hashed. Since mempool is a hash array, no 
        need to hash it again */
+    IntTypes int_type = IntTypes();
     uint64_t SingleMempoolHash[8];
     memcpy(SingleMempoolHash, sha512("abc"), sizeof(uint64_t)<<3);
-    
-    IntTypes bigInt = IntTypes();
-    bigInt.__uint256_t(SingleMempoolHash);
+    for(uint64_t c : SingleMempoolHash) {
+        std::cout << std::hex << c << " ";
+    }
+    int_type.__uint256_t(SingleMempoolHash);
     return 0;
 }
