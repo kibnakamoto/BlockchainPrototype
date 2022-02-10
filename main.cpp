a16462a6e9782dfa148511fd303b0be607ac67d3
kibnakamoto <86175813+kibnakamoto@users.noreply.github.com>
2022-02-10 18:12:59 -0500

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -6,7 +6,7 @@
 
 #include <iostream>
 #include <string>
-#include "__uint256_t.h"
+#include "bigInt.h"
 #include "sha512.h"
 
 int main()
@@ -15,15 +15,20 @@ int main()
        with the that needs to be hashed. Since mempool is a hash array, no 
        need to hash it again */
     IntTypes int_type = IntTypes();
-    uint64_t SingleMempoolHash[8];
-    memcpy(SingleMempoolHash, sha512("abc"), sizeof(uint64_t)<<3);
-    for(uint64_t c : SingleMempoolHash) {
+    uint64_t SingleMempoolHash64[8];
+    uint32_t SingleMempoolHash32[8] {0xddaf35a1, 0x93617aba, 0xcc417349, 
+                                     0xae204131, 0x12e6fa4e, 0x89a97ea2,
+                                     0x0a9eeee6, 0x4b55d39a};
+    memcpy(SingleMempoolHash64, sha512("abc"), sizeof(uint64_t)<<3);
+    for(uint64_t c : SingleMempoolHash64) {
         std::cout << std::hex << c << " ";
     }
-    auto [fst, snd] = int_type.__uint256_t(SingleMempoolHash);
+    auto [fst, snd, trd, frd] = int_type.__uint512_t(SingleMempoolHash64);
     for(int c=0;c<0;c++) {
         fst;
         snd;
+        trd;
+        frd;
     }
 
     
