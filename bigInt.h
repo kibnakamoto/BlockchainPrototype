105bea7826aa0d7d1aed978acc67b58342a3a25e
kibnakamoto <86175813+kibnakamoto@users.noreply.github.com>
2022-02-10 18:22:47 -0500

    Update bigInt.h

--- bigInt.h
+++ bigInt.h
@@ -22,18 +22,36 @@ class IntTypes
         /* this creates a tuple of 4 variables that are 128-bit each and 
            stores 512-bit unsigned int data */
         inline std::tuple<__uint128_t, __uint128_t,__uint128_t, __uint128_t>
-                         __uint512_t(uint64_t mempoolSingleHash[8])
+        __uint512_t(uint64_t mempoolSingleHash[8])
         {
             __uint128_t arr128[8>>1];
             
-            // convert each 4 32-bit unsigned int to 1 128-bit unsigned int
+            // convert each 2 64-bit unsigned int to 1 128-bit unsigned int
             for(int c=0;c<4;c++) {
                 arr128[c] = (((__uint128_t)mempoolSingleHash[c*2]<< 64) |
                              ((__uint128_t)mempoolSingleHash[c*2+1]));
             }
             return {arr128[0], arr128[1], arr128[2], arr128[3]};
         }
-        // TODO: define 1024 bit int for 2 distinct mempoolSingleHash arrays. 
-        // Use this in the sha512 merkle tree.
         
+        // converts 2 array[8] of 64-bit unsigned int into 1 1024 bit tuple
+        inline std::tuple<__uint128_t, __uint128_t,__uint128_t, __uint128_t,
+                          __uint128_t, __uint128_t,__uint128_t, __uint128_t>
+        __uint1024_t(uint64_t mempoolSingleHash1[8], uint64_t mempoolSingleHash2[8])
+        {
+            // test function
+            __uint128_t arr128[8];
+            uint64_t hashArr[8<<1];
+            for(int c=0;c<8;c++) {
+                hashArr[c] = mempoolSingleHash1[c];
+                hashArr[c+8] = mempoolSingleHash2[c];
+            }
+            // convert each 4 32-bit unsigned int to 1 128-bit unsigned int
+            for(int c=0;c<8;c++) {
+                arr128[c] = (((__uint128_t)hashArr[c*2]<< 64) |
+                             ((__uint128_t)hashArr[c*2+1]));
+            }
+            return {arr128[0], arr128[1], arr128[2], arr128[3], arr128[4], 
+                    arr128[5], arr128[6], arr128[7]};
+        }
 };
