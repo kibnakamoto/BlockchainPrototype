d3ab9c13e6b84f6dc8c198e25997d0d10cb618e8
kibnakamoto <86175813+kibnakamoto@users.noreply.github.com>
2022-02-18 11:11:12 -0500

    Update MerkleTree.h

--- MerkleTree.h
+++ MerkleTree.h
@@ -9,31 +9,48 @@ class MerkleTree
         std::vector<uint64_t*> mempool; // each index of pointer has length of 8
     
     public:
-        std::string MerkleRoot()
+        uint64_t* MerkleRoot()
         {
             IntTypes int_type = IntTypes();
             mempool.push_back(sha512("sender: N/A-receiver: N/A-amount: N/A"));
+
             __uint128_t len = mempool.size();
             for(int c=0;c<8;c++) {
-                std::cout << std::hex << mempool[0][c] << " ";
+                // std::cout << std::hex << mempool[0][c] << " ";
 // 378882b8368ece7f2fa8f83802cd5c36f7e0390f8512ba24d7e6a7edd9bbd907d15934b5c1fc5
 // f29dfc1ba8f8b74eb078f8cb2251a865170d184f7ebe81b3a02
             }
-            for(__uint128_t c=len;c>len/2;c--) { // TODO: fix
-                
+            bool odd = true;
+            uint64_t amount_of_leafts = 0;
+            __uint128_t tmp_leaf_len = len;
+            if(len%2 != odd) {
+                while(tmp_leaf_len>1) {
+                    tmp_leaf_len -= 2;
+                    amount_of_leafts += 1;
+                }
+            } else {
+                while(tmp_leaf_len>0) {
+                    if(tmp_leaf_len !=1) {
+                    tmp_leaf_len--;tmp_leaf_len--;
+                    } else {
+                        tmp_leaf_len--;
+                    }
+                    amount_of_leafts += 1;
+                }
             }
             
-            // calculate MerkleTree
+            // calculate MerkleRoot
             for(__uint128_t c=0;c<len;c++) {
-                for(int c=0;c<8;c++) {
-                    if (len%2 == 0) {
-                        
-                    } else {
+                for(int i=0;i<8;i++) {
+                    if (len%2 == odd) {
+                        //  = mempool[c][i];
+                    } 
+                    else if(len%2 != odd) {
                         
                     }
                 }
             }
-            return std::string();
+            return nullptr;
         }
 };
 
