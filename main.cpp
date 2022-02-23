2b7dcd9ee61f371b6fab6c8404adcdf04181460a
kibnakamoto <86175813+kibnakamoto@users.noreply.github.com>
2022-02-23 09:24:21 -0500

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,6 +1,11 @@
 /*
-* This implementation is for modern c++. Some of the methods used are after c++17
-* so if you are using an older version, make some tweaks.
+* Author: Taha Canturk
+*  Github: kibnakamoto
+*   Start Date: Feb 9, 2022
+*    Finish Date: N/A
+*
+* This implementation only works for C++ version c++17 or above. 
+* C++ 14 also works but gives warning
 * 
 */
 
@@ -8,21 +13,31 @@
 #include <string>
 #include "bigInt.h"
 #include "sha512.h"
+#include "MerkleTree.h"
+
+struct SingleMempoolHash64 {
+    
+};
 
 int main()
 {
-    /* iterate each value of mempool using this method, replace string abc 
-       with the that needs to be hashed. Since mempool is a hash array, no 
-       need to hash it again */
+    /* need string hash values while comparing hashes */
     IntTypes int_type = IntTypes();
+    MerkleTree merkle_tree = MerkleTree();
     uint64_t SingleMempoolHash64[8];
-    uint32_t SingleMempoolHash32[8] {0xddaf35a1, 0x93617aba, 0xcc417349, 
-                                     0xae204131, 0x12e6fa4e, 0x89a97ea2,
-                                     0x0a9eeee6, 0x4b55d39a};
-    memcpy(SingleMempoolHash64, sha512("abc"), sizeof(uint64_t)<<3);
+    memcpy(SingleMempoolHash64, sha512("sender: N/A-receiver: N/A-amount: N/A"),
+           sizeof(uint64_t)<<3);
     for(uint64_t c : SingleMempoolHash64) {
         std::cout << std::hex << c << " ";
     }
+    std::vector<uint64_t*> mempool;
+    uint64_t singleHash[8];
+    
+    memcpy(singleHash,sha512("sender: N/A-receiver: N/A-amount: N/A"), 64);
+    mempool.push_back(singleHash);
+    merkle_tree.MerkleRoot(mempool);
+    
+    // 8x64 bit transaction hash into 4x128 transaction hash
     auto [fst, snd, trd, frd] = int_type.__uint512_t(SingleMempoolHash64);
     for(int c=0;c<0;c++) {
         fst;
