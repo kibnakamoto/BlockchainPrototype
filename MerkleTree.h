a5aacf18c3296f5514552cb153a7ebe7fdfc3c3f
kibnakamoto <86175813+kibnakamoto@users.noreply.github.com>
2022-02-23 09:23:45 -0500

    Update MerkleTree.h

--- MerkleTree.h
+++ MerkleTree.h
@@ -5,22 +5,11 @@
 
 class MerkleTree
 {
-    protected:
-        std::vector<uint64_t*> mempool; // each index of pointer has length of 8
-    
     public:
-        uint64_t* MerkleRoot()
+        uint64_t* MerkleRoot(std::vector<uint64_t*> mempool)
         {
             IntTypes int_type = IntTypes();
-            uint64_t singleHash[8];
-            memcpy(singleHash,sha512("sender: N/A-receiver: N/A-amount: N/A"), 64);
-            mempool.push_back(singleHash);
-            
             __uint128_t len = mempool.size();
-            for(int c=0;c<8;c++) {
-                uint64_t x = mempool.at(0)[c];
-                std::cout << std::hex << x << " ";
-            }
             bool odd = true;
             
             // calculate MerkleRoot
