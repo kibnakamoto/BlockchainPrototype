30f7213e5101615fddd298b404bdfd09b489cff8
kibnakamoto <86175813+kibnakamoto@users.noreply.github.com>
2022-02-24 10:35:40 -0500

    Update MerkleTree.h

--- MerkleTree.h
+++ MerkleTree.h
@@ -9,21 +9,29 @@ class MerkleTree
         uint64_t* MerkleRoot(std::vector<uint64_t*> mempool)
         {
             IntTypes int_type = IntTypes();
-            __uint128_t len = mempool.size();
+            uint64_t len = mempool.size();
             bool odd = true;
-            
-            // calculate MerkleRoot
-            for(__uint128_t c=0;c<len;c++) {
-                for(int i=0;i<8;i++) {
-                    if (len%2 == odd) {
-                        //  = mempool[c][i];
-                    } 
-                    else if(len%2 != odd) {
-                        
+            uint64_t* merkle_root;
+            if(len == 1) { // if len 1, use the same hash twice to get merkle_root
+                std::string SINGLE_HASH_MEMLEN1="";
+                for(int c=0;c<8;c++) {
+                    SINGLE_HASH_MEMLEN1 += std::to_string(mempool[0][c]);
+                }
+                merkle_root = sha512(SINGLE_HASH_MEMLEN1 + SINGLE_HASH_MEMLEN1);
+            } else {
+                // calculate MerkleRoot
+                for(uint64_t c=0;c<len;c++) {
+                    for(int i=0;i<8;i++) {
+                        if (len%2 == odd) {
+                            
+                        }
+                        else if(len%2 != odd) {
+                            
+                        }
                     }
                 }
             }
-            return nullptr;
+            return merkle_root;
         }
 };
 
