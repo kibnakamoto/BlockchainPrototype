a963fe0a0352762403606ad0a749195aa71d8158
kibnakamoto <86175813+kibnakamoto@users.noreply.github.com>
2022-02-27 15:39:29 -0500

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -4,7 +4,7 @@
 *   Start Date: Feb 9, 2022
 *    Finish Date: N/A
 *
-* This implementation only works for C++ version c++17 or above. 
+* This implementation only works for C++ version 17 or above. 
 * C++ 14 also works but gives warning
 * 
 */
@@ -15,9 +15,34 @@
 #include "sha512.h"
 #include "MerkleTree.h"
 
-struct SingleMempoolHash64 {
+struct SingleMempoolHash {
+    uint64_t* sender;
+    uint64_t* receiver;
+    uint32_t amount;
     
+    // A single hashed transaction data
+    uint64_t* transactionHash()
+    {
+        std::string transactionData = "";
+        transactionData += "sender: ";
+        for(int c=0;c<8;c++) {
+            transactionData += std::to_string(sender[c]) + " ";
+        }
+        std::cout << transactionData; // wrong, TODO: fix
+        transactionData += ", receiver: ";
+        for(int c=0;c<8;c++) {
+            transactionData += std::to_string(receiver[c]) + " ";
+        }
+        transactionData += ", amount: " + std::to_string(amount);
+        
+        return sha512(transactionData);
+    }
 };
+
+uint64_t* GenerateNewWalletAddress(uint64_t* private_key, uint64_t* public_key)
+{
+    return nullptr;
+}
 
 int main()
 {
@@ -25,17 +50,19 @@ int main()
     IntTypes int_type = IntTypes();
     MerkleTree merkle_tree = MerkleTree();
     uint64_t SingleMempoolHash64[8];
-    uint64_t merkle_root[8];
+    uint64_t merkle_root[8]; // declare Merkle Root
+    
+    /* Sample Hash Value */
     memcpy(SingleMempoolHash64, sha512("sender: N/A-receiver: N/A-amount: N/A"),
            sizeof(uint64_t)<<3);
     for(uint64_t c : SingleMempoolHash64) {
         std::cout << std::hex << c << " ";
     }
-    std::vector<uint64_t*> mempool;
-    uint64_t singleHash[8];
+    std::vector<uint64_t*> mempool; // declare mempool
     
-    memcpy(singleHash,sha512("sender: N/A-receiver: N/A-amount: N/A"), 64);
-    mempool.push_back(singleHash);
+    struct SingleMempoolHash transaction{sha512("sender"), sha512("receiver"),
+                                         50000};
+    mempool.push_back(transaction.transactionHash());
     merkle_tree.MerkleRoot(mempool, merkle_root);
     
     // 8x64 bit transaction hash into 4x128 transaction hash
@@ -46,7 +73,5 @@ int main()
         trd;
         frd;
     }
-    
-    
     return 0;
 }
