3cdbd3e96c0f3c9b78f0f55d7b6b909e0e3ddb2b
kibnakamoto <86175813+kibnakamoto@users.noreply.github.com>
2022-03-14 13:49:29 -0400

    Update MerkleTree.h

--- MerkleTree.h
+++ MerkleTree.h
@@ -1,28 +1,54 @@
-
 #include <stdint.h>
 #include <vector>
 #include <string>
 #include "sha512.h"
+
 class MerkleTree
 {
     public:
-        uint64_t* MerkleRoot(std::vector<uint64_t*> mempool, uint64_t* merkle_root)
+        void MerkleRoot(std::vector<uint64_t*> mempool, uint64_t* merkle_root)
         {
             IntTypes int_type = IntTypes();
             SHA512 hash = SHA512();
             uint64_t len = mempool.size();
+            uint64_t currlen = len;
             bool odd = true;
-            // calculate MerkleRoot
-            for(uint64_t c=0;c<len;c++) {
-                for(int i=0;i<8;i++) {
-                    if (len%2 == odd) {
-                        
-                    }
-                    else if(len%2 != odd) {
-                        
-                    }
-                }
+            bool divisible;
+            if(len%2 == odd) { // make sure len is not odd
+                uint64_t* oddZfill = new uint64_t[8];
+                oddZfill = sha512("00000000");
+                mempool.push_back(oddZfill);
+                len++;
             }
-            return merkle_root;
+            uint64_t divCurrlen = len;
+            while(divisible != true) {
+                uint64_t* oddZfill = new uint64_t[8];
+                oddZfill = sha512("00000000");
+                divCurrlen/=2;
+                if(divCurrlen%2 != 0) {
+                    mempool.push_back(oddZfill);
+                    len++;
+                }
+                divisible = (divCurrlen == 0);
+                mempool.push_back(oddZfill);
+                len++;
+                std::cout << std::dec << divCurrlen << " ";
+            }
+            std::cout << "\nlen:\t" << std::dec << len << "\n\n";
+            /*
+             * create newlen for adding zero hashes whenever there aren't enough
+             * leaves in the creation of the MerkleRoot. Do not add it to mempool
+             */
+            // if length not a multiple of 4, there aren't enough leaves
+            uint64_t j=0;
+            
+            // calculate MerkleRoot
+            // while(currlen >= 0) {
+                
+                
+                // update current length of leaves until MerkleRoot
+            //     currlen /= 2;
+            //     j++;
+            // }
         }
 };
