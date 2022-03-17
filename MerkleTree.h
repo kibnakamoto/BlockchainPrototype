1b58c9871dd017bd7b86074ad2a49731e3de60c9
kibnakamoto <86175813+kibnakamoto@users.noreply.github.com>
2022-03-17 12:55:17 -0400

    Update MerkleTree.h

--- MerkleTree.h
+++ MerkleTree.h
@@ -1,4 +1,3 @@
-
 #include <stdint.h>
 #include <vector>
 #include <string>
@@ -15,36 +14,36 @@ namespace MerkleTree
             uint64_t len = mempool.size();
             uint64_t currlen = len;
             bool odd = true;
-            bool divisible;
             if(len%2 == odd) { // make sure len is not odd
                 uint64_t* oddZfill = new uint64_t[8];
                 oddZfill = sha512("00000000");
                 mempool.push_back(oddZfill);
                 len++;
             }
-            uint64_t divCurrlen = len;
+            // uint64_t divCurrlen = len;
             bool divs;
-            int amountofLoop = 0;
-            while (divCurrlen != 0) {
-                divCurrlen /= 2;
-                amountofLoop++;
-                for(int c=0;c<amountofLoop*2;c++) {
-                    uint64_t* oddZfill = new uint64_t[8];
-                    oddZfill = sha512("00000000");
-                    mempool.push_back(oddZfill);
-                    len++; // update len
-                }
-                divs = (divCurrlen/2) % 2 == 0;
-                if(divs != true) {
-                    mempool.push_back(oddZfill);
-                    len++; // update len
-                }
-                std::cout << "\n\n" << divCurrlen << "\n\n";
+            __uint128_t validlen = 2;
+            // while (divCurrlen != 0) {
+            //     divCurrlen /= 2;
+            //     divs = (divCurrlen) % 2 == 0;
+            //     if(divs != true) {
+            //         // divCurrlen++;
+            //     }
+            while(validlen <= len) {
+                validlen*=2;
             }
-            std::cout << "\nlen:\t" << std::dec << len << "\n\n";
+            
+            while(len<validlen) { // append it 2, 4, 8... times
+                uint64_t* oddZfill = new uint64_t[8];
+                oddZfill = sha512("00000000");
+                mempool.push_back(oddZfill);
+                len++; // update len
+            }
+                // std::cout << "\n\n" << divCurrlen << "\n\n";
+            std::cout << "\nlen:\t" << std::dec << (uint64_t)validlen << "\n\n";
             
             // calculate MerkleRoot
-            // while(currlen >= 0) {
+            // while(currlen != 0 || currlen != 1) {
                 
                 
                 // update current length of leaves until MerkleRoot
@@ -53,5 +52,3 @@ namespace MerkleTree
             // }
         }
 }; // namespace MerkleTree
-
-
