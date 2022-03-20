290e5478b21d9d5b24a85c469ea7831f0f1bf619
kibnakamoto <86175813+kibnakamoto@users.noreply.github.com>
2022-03-20 18:44:22 -0400

    Update MerkleTree.h

--- MerkleTree.h
+++ MerkleTree.h
@@ -1,13 +1,14 @@
 #include <stdint.h>
 #include <vector>
 #include <string>
+#include <memory>
 #include "sha512.h"
 
 namespace MerkleTree
 {
-        std::vector<uint64_t*> merkleRoots;
+        std::vector<std::shared_ptr<uint64_t>> merkleRoots;
         
-        inline uint64_t length(std::vector<uint64_t*> mempool)
+        inline uint64_t length(std::vector<std::shared_ptr<uint64_t>> mempool)
         {
             return mempool.size();
         }
@@ -15,49 +16,52 @@ namespace MerkleTree
         class Node
         {
             private:
-                std::vector<uint64_t*> append_level(std::vector<uint64_t*> Plevel,
-                                                    uint64_t len)
+                std::vector<std::shared_ptr<uint64_t>> append_level(std::vector
+                                                                    <std::shared_ptr
+                                                                    <uint64_t>>
+                                                                    Plevel,
+                                                                    uint64_t len)
                 {
                     SHA512 hash = SHA512();
-                    std::vector<uint64_t*> nodes;
-                    for(uint64_t r=0;r<len/2;r++) {
-                            nodes.push_back(hash.sha512_ptr(Plevel[r*2],
-                                                            Plevel[r*2+1]));
+                    std::vector<std::shared_ptr<uint64_t>> nodes;
+                    for(double r=0;r<len/2;r++) {
+                            nodes.push_back(hash.sha512_ptr(Plevel[(uint64_t)(r*2)],
+                                                            Plevel[(uint64_t)(r*2+1)]));
                     }
                     /* nodes are single a layer of the MerkleTree */
                     /* update Plevel to level in another function until len = 1 */
                     return nodes;
                 }
             public:
-                void append_levels(std::vector<uint64_t*> mempool, uint32_t
-                                   amountofLayers, uint64_t len, uint64_t*
-                                   merkle_root)
+                void append_levels(std::vector<std::shared_ptr<uint64_t>> mempool, uint64_t len,
+                                   std::shared_ptr<uint64_t> merkle_root)
                 {
                     uint64_t currlen = len;
-                    std::vector<uint64_t*> level = mempool;
+                    std::vector<std::shared_ptr<uint64_t>> level = mempool;
                     while(currlen != 1) {
                         level = append_level(level, currlen);
                         currlen/=2;
                     } if(level.size() == 1) {
-                        merkle_root = level[0];
+                        merkle_root = std::move(std::shared_ptr<uint64_t>
+                                                (level[0]));
                         std::cout << "\ncondition met\n"; /* test a few times */
                     } else {
                         std::cout << "error, condition not met";
-                    }
-                    for(int c=0;c<8;c++) {
-                        std::cout << std::hex << merkle_root[c] << " ";
+                        exit(EXIT_FAILURE);
                     }
                 }
         };
         
-        inline void merkleRoot(std::vector<uint64_t*> Mempool, uint64_t* merkle_root)
+        inline void merkleRoot(std::vector<std::shared_ptr<uint64_t>> Mempool,
+                               std::shared_ptr<uint64_t> merkle_root)
         {
             IntTypes int_type = IntTypes();
             SHA512 hash = SHA512();
             Node node = Node();
             
             // to avoid 0 hashes to be invalid transactions in Mempool
-            std::vector<uint64_t*> mempool = Mempool;
+            std::vector<std::shared_ptr<uint64_t>> mempool = Mempool;
+            
             uint64_t len = mempool.size(); // amount of transactions in the block
             uint64_t validlen = 2;
             uint32_t amountofLayers = 0;
@@ -66,7 +70,7 @@ namespace MerkleTree
             }
             
             while(len<validlen) { // append it 2, 4, 8... times
-                uint64_t* oddZfill = new uint64_t[8];
+                std::shared_ptr<uint64_t> oddZfill(new uint64_t[8]);
                 oddZfill = sha512("00000000");
                 mempool.push_back(oddZfill);
                 len++; // update len
@@ -78,18 +82,10 @@ namespace MerkleTree
                 amountofLayers++;
                 /* validlen gets set to zero so don't use it after this loop */
             }
-            node.append_levels(mempool, amountofLayers, len, merkle_root);
             // calculate MerkleRoot
-            uint64_t currlen = len;
-            while(currlen != 1) {
-                // update current length of leaves until MerkleRoot
-                currlen /= 2;
-                
-                // calculate leaf
-                for(int i=0;i<currlen;i++) {
-                    hash.sha512_ptr(mempool[i], mempool[i+1]);
-                }
-            }
+            node.append_levels(mempool, len, merkle_root);
+            
+            // store merkle_root in vector merkleRoots
             merkleRoots.push_back(merkle_root);
         }
 }; // namespace MerkleTree
