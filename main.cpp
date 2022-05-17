851698305be39aa6de4db4cec85cad1b80c2d593
kibnakamoto <86175813+kibnakamoto@users.noreply.github.com>
2022-05-17 16:29:15 -0400

    update copyright notice

--- main.cpp
+++ main.cpp
@@ -1,7 +1,17 @@
-// Copyright (c) 2022 Taha Canturk
-// Distributed under the GPL-3.0 software license, see the accompanying
-// file COPYING or https://www.gnu.org/licenses/gpl-3.0.en.html
- 
+/*
+ * A Blockchain Prototype that mimics the functions of a blockchain. Around 3500 lines of code
+ * Copyright (C) 2022 Taha Canturk
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *   This program is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *   GNU General Public License for more details.
+ *    You should have received a copy of the GNU General Public License
+ *    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
 
 /* Author: Taha Canturk
  *  Github: kibnakamoto
